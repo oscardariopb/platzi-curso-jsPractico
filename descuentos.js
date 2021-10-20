0af5acea1a7c9dc38357893b72fb215b899075fc
= <cronos1610@hotmail.com>
2021-10-19 21:42:08 -0500

    finalizada parte 1 del taller 2, cupones solución uno a través del input de descuento simple

--- descuentos.js
+++ descuentos.js
@@ -1,6 +1,12 @@
 //const precioOriginal = 120;
 //const descuento = 18;
 
+const coupons = [
+    "JuanDC_es_Batman",
+    "pero_no_le_digas_a_nadie",
+    "es_un_secreto",
+];
+
 function calcularPrecioConDescuento(precio, descuento) {
 
     const porcentajePrecioConDescuento = 100 - descuento;
@@ -9,7 +15,7 @@ function calcularPrecioConDescuento(precio, descuento) {
     return precioConDescuento;
 };
 
-function onClickButtonPriceDiscount() {
+/* function onClickButtonPriceDiscount() {
     const inputPrice = document.getElementById('InputPrice');
     const priceValue = inputPrice.value;
     
@@ -21,8 +27,34 @@ function onClickButtonPriceDiscount() {
     const resultP = document.getElementById('ResultP');
     resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
     
-};
+}; */
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
 
+    let descuento;
+
+    switch(couponValue) {
+        case coupons[0]: // "JuanDC_es_Batman"
+        descuento = 15;
+        break;
+        case coupons[1]: // "pero_no_le_digas_a_nadie"
+        descuento = 30;
+        break;
+        case coupons[2]: // "es_un_secreto"
+        descuento = 25;
+        break;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+    
+};
 
 
 // console.log({
